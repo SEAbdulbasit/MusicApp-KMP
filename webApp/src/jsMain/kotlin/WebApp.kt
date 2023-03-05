aa2464b1908c6f5854b5f3d597f294d38b62d121
Abdul Basit <abdulbasit5361234@gmail.com>
2023-03-05 11:31:18 +0500

    Increase Canvas size to fill screen

--- webApp/src/jsMain/kotlin/WebApp.kt
+++ webApp/src/jsMain/kotlin/WebApp.kt
@@ -1,10 +1,15 @@
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Window
 import org.jetbrains.skiko.wasm.onWasmReady
 
 fun main() {
     onWasmReady {
         Window("MusicApp-KMP") {
-            CommonMainWeb()
+            Column(modifier = Modifier.fillMaxSize()) {
+                CommonMainWeb()
+            }
         }
     }
 }

9d42b5299d080f700e87b8d982caf1a016c96c50
Abdul Basit <abdulbasit5361234@gmail.com>
2023-04-02 21:54:21 +0500

    Add decompose for web

--- shared/src/jsMain/kotlin/main.js.kt
+++ shared/src/jsMain/kotlin/main.js.kt
@@ -5,8 +5,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import com.example.musicapp_kmp.MainCommonLarge
-import com.example.musicapp_kmp.player.MediaPlayerController
+import com.example.musicapp_kmp.MainCommon
+import com.example.musicapp_kmp.decompose.MusicRoot
 import com.seiko.imageloader.ImageLoader
 import com.seiko.imageloader.LocalImageLoader
 import com.seiko.imageloader.cache.memory.maxSizePercent
@@ -14,7 +14,7 @@ import com.seiko.imageloader.component.setupDefaultComponents
 
 
 @Composable
-fun CommonMainWeb() {
+fun CommonMainWeb(root: MusicRoot) {
     CompositionLocalProvider(
         LocalImageLoader provides ImageLoader {
             components {
@@ -28,7 +28,7 @@ fun CommonMainWeb() {
         },
     ) {
         Box(Modifier.background(color = Color(0xFF1A1E1F)).fillMaxSize()) {
-            MainCommonLarge(MediaPlayerController())
+            MainCommon(root, true)
         }
     }
 }
--- webApp/src/jsMain/kotlin/WebApp.kt
+++ webApp/src/jsMain/kotlin/WebApp.kt
@@ -1,10 +1,25 @@
 import androidx.compose.ui.window.Window
+import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import com.arkivanov.essenty.lifecycle.resume
+import com.example.musicapp_kmp.decompose.MusicRootImpl
+import com.example.musicapp_kmp.network.SpotifyApiImpl
+import com.example.musicapp_kmp.player.MediaPlayerController
 import org.jetbrains.skiko.wasm.onWasmReady
 
 fun main() {
     onWasmReady {
+        val lifecycle = LifecycleRegistry()
+        val rootComponent =
+            MusicRootImpl(
+                componentContext = DefaultComponentContext(
+                    lifecycle = lifecycle,
+                ), api = SpotifyApiImpl(), mediaPlayerController = MediaPlayerController()
+            )
+
+        lifecycle.resume()
         Window("MusicApp-KMP") {
-            CommonMainWeb()
+            CommonMainWeb(rootComponent)
         }
     }
 }

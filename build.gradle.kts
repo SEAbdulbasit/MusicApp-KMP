05c79389dcc35ca99f6c121ea57a64e7a07c4ad9
ranger163 <ahmed.nassar163@gmail.com>
2023-05-04 16:48:09 +0300

    - Upgrading AGP dependency from 7.4.0-beta02 to 7.4.0 - Removing "kotlin.mpp.enableGranularSourceSetsMetadata = true" as it has no effect in this and future Kotlin versions, as Hierarchical Structures support is now enabled by default. - Removing "kotlin.native.enableDependencyPropagation = false" as has no effect in this and future Kotlin versions, as Kotlin/Native dependency commonization is now enabled by default.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
+buildscript {
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.4.0")
+    }
+}
 plugins {
-    id("com.android.application").version("7.4.0-beta02").apply(false)
-    id("com.android.library").version("7.4.0-beta02").apply(false)
+    id("com.android.application").version("7.4.0").apply(false)
+    id("com.android.library").version("7.4.0").apply(false)
     id("org.jetbrains.compose").version("1.3.0") apply false
     kotlin("android").version("1.8.0").apply(false)
     kotlin("multiplatform").version("1.8.0").apply(false)

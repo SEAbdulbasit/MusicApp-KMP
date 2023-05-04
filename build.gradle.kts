70796329c4cd3203c3e9ca55591037f848d2d885
ranger163 <ahmed.nassar163@gmail.com>
2023-05-04 17:47:36 +0300

    - Removing buildscript android tools classPath.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,3 @@
-buildscript {
-    dependencies {
-        classpath("com.android.tools.build:gradle:7.4.0")
-    }
-}
 plugins {
     id("com.android.application").version("7.4.0").apply(false)
     id("com.android.library").version("7.4.0").apply(false)

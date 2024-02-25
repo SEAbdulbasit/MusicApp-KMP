de7569c60ad22cddd2f13f21e2c562c9f068cea6
Abdul Basit <abdulbasit5361234@gmail.com>
2024-02-25 16:57:35 +0500

    send dummy response if token is null

--- shared/src/commonMain/kotlin/com/example/musicapp_kmp/Token.kt
+++ shared/src/commonMain/kotlin/com/example/musicapp_kmp/Token.kt
@@ -5,5 +5,4 @@ package com.example.musicapp_kmp
  * Created by abdulbasit on 26/02/2023.
  */
 
-const val TOKEN =
-    "Bearer BQB1ObL_oZaUHdRIdJ7zKF9UGTQ-DwCNYqYsVzepHVeQlSUUeLH5A3DxBtVCJUySvwBVcso1pn2I8wnYfr3lyBQ0AmSCCCcfliwtYlZOk7DJRZwHK_OouvPIkGM6qNaNnw3pjkgkbrtg6G095-lAksHMYRFMx763W4V6LV1JDQ29n1VV8n9e6xB1lwvdrjb6xeqtl2yYGTFfQsfn5co"
+const val TOKEN =""
--- shared/src/commonMain/kotlin/com/example/musicapp_kmp/network/SpotifyApiImpl.kt
+++ shared/src/commonMain/kotlin/com/example/musicapp_kmp/network/SpotifyApiImpl.kt
@@ -1,9 +1,11 @@
 package com.example.musicapp_kmp.network
 
 import com.example.musicapp_kmp.TOKEN
+import com.example.musicapp_kmp.dummydata.topFiftyChartsResponse
 import com.example.musicapp_kmp.network.models.featuredplaylist.FeaturedPlayList
 import com.example.musicapp_kmp.network.models.newreleases.NewReleasedAlbums
 import com.example.musicapp_kmp.network.models.topfiftycharts.TopFiftyCharts
+import featurePlaylistResponse
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.plugins.*
@@ -11,13 +13,18 @@ import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+import newReleases
 
 
 /**
  * Created by abdulbasit on 26/02/2023.
- */
+// */
 class SpotifyApiImpl : SpotifyApi {
     override suspend fun getTopFiftyChart(): TopFiftyCharts {
+        if (TOKEN.isEmpty()) {
+            return Json.decodeFromString<TopFiftyCharts>(topFiftyChartsResponse)
+        }
         return client.get {
             headers {
                 sptifyEndPoint("v1/playlists/37i9dQZEVXbMDoHDwVN2tF")
@@ -26,6 +33,9 @@ class SpotifyApiImpl : SpotifyApi {
     }
 
     override suspend fun getNewReleases(): NewReleasedAlbums {
+        if (TOKEN.isEmpty()) {
+            return Json.decodeFromString<NewReleasedAlbums>(newReleases)
+        }
         return client.get {
             headers {
                 sptifyEndPoint("v1/browse/new-releases")
@@ -34,6 +44,9 @@ class SpotifyApiImpl : SpotifyApi {
     }
 
     override suspend fun getFeaturedPlaylist(): FeaturedPlayList {
+        if (TOKEN.isEmpty()) {
+            return Json.decodeFromString<FeaturedPlayList>(featurePlaylistResponse)
+        }
         return client.get {
             headers {
                 sptifyEndPoint("v1/browse/featured-playlists")
@@ -42,6 +55,9 @@ class SpotifyApiImpl : SpotifyApi {
     }
 
     override suspend fun getPlayList(playlistId: String): TopFiftyCharts {
+        if (TOKEN.isEmpty()) {
+            return Json.decodeFromString<TopFiftyCharts>(topFiftyChartsResponse)
+        }
         return client.get {
             headers {
                 sptifyEndPoint("v1/playlists/$playlistId")
@@ -58,7 +74,7 @@ class SpotifyApiImpl : SpotifyApi {
             socketTimeoutMillis = timeout
         }
         install(ContentNegotiation) {
-            json(kotlinx.serialization.json.Json { isLenient = true; ignoreUnknownKeys = true })
+            json(Json { isLenient = true; ignoreUnknownKeys = true })
         }
     }
 
@@ -72,6 +88,5 @@ class SpotifyApiImpl : SpotifyApi {
                 )
             }
         }
-
     }
 }

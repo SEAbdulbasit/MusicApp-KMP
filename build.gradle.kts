d4572c1ad3a2466c0199f3dc073bed45bb3d09f3
Sushant Hande <sushanthande1@gmail.com>
2024-05-11 20:23:42 +0530

    Added support for accessing resources in commonMain

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application").version("7.4.0").apply(false)
     id("com.android.library").version("7.4.0").apply(false)
-    id("org.jetbrains.compose").version("1.5.12") apply false
+    id("org.jetbrains.compose").version("1.6.2") apply false
     kotlin("android").version("1.8.0").apply(false)
     kotlin("multiplatform").version("1.9.22").apply(false)
     kotlin("jvm") apply false
